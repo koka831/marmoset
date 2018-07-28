07eeada6cf7c24a6e3b6632a2a3ca9b4ee5c6c7a
koka <koka.code@gmail.com>
2018-07-28 17:57:27 +0900

    chore: Option<chr>

--- lib/lexer/lexer.rs
+++ lib/lexer/lexer.rs
@@ -7,7 +7,7 @@ pub struct Lexer {
     input: Vec<char>,
     pos: usize, // current position in input (points to current char)
     read_pos: usize, // current reading position (after current char)
-    chr: char,
+    chr: Option<char>,
 }
 
 impl Lexer {
@@ -16,24 +16,25 @@ impl Lexer {
             input: s.chars().collect::<Vec<char>>(),
             pos: 0,
             read_pos: 0,
-            chr: '0'
+            chr: None,
         }
     }
 
     pub fn next_token(&mut self) -> Token {
-        let tok = Token::new(&self.input[self.pos]);
-        self.pos += 1;
+        let chr = self.read_char();
+        let tok = Token::new(&chr);
         tok
     }
 
-    fn read_char(&mut self) {
+    fn read_char(&mut self) -> Option<char> {
         // check if it reached the end of input.
         if self.read_pos >= self.input.len() {
-            self.chr = '0';
+            self.chr = None;
         } else {
-            self.chr = self.input[self.read_pos];
+            self.chr = Some(self.input[self.read_pos]);
         }
         self.pos = self.read_pos;
         self.read_pos += 1;
+        self.chr
     }
 }
--- lib/lexer/token.rs
+++ lib/lexer/token.rs
@@ -29,17 +29,19 @@ pub enum Token {
 
 
 impl Token {
-    pub fn new(c: &char) -> Self {
-        match &c {
-            '=' => Token::Assign,
-            '+' => Token::Plus,
-            ',' => Token::Comma,
-            ';' => Token::Semicolon,
-            '(' => Token::LParen,
-            ')' => Token::RParen,
-            '{' => Token::LBrace,
-            '}' => Token::RBrace,
-            _   => Token::Illegal,
-        }
+    pub fn new(c: &Option<char>) -> Self {
+        if let Some(c) = c {
+            match &c {
+                '=' => Token::Assign,
+                '+' => Token::Plus,
+                ',' => Token::Comma,
+                ';' => Token::Semicolon,
+                '(' => Token::LParen,
+                ')' => Token::RParen,
+                '{' => Token::LBrace,
+                '}' => Token::RBrace,
+                _   => Token::Illegal,
+            }
+        } else { Token::EOF }
     }
 }
